38d2598c8ce0b1018550754dba03da2382a3a5b9
linhdh <doanhoai.linh1123@gmail.com>
2024-07-19 13:56:32 +0700

    add package

---
+++ focr-sdk-ios/Sources/FOCR.xcframework/ios-arm64_x86_64-simulator/FOCR.framework/Headers/FOCR.h
@@ -0,0 +1,18 @@
+//
+//  FOCR.h
+//  FOCR
+//
+//  Created by Linh on 13/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FOCR.
+FOUNDATION_EXPORT double FOCRVersionNumber;
+
+//! Project version string for FOCR.
+FOUNDATION_EXPORT const unsigned char FOCRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FOCR/PublicHeader.h>
+
+
